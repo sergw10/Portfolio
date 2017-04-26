bd632014a0744359fd52c99759416a94a4b2f9a4
Sergey Rubezhin <sergw100@gmail.com>
2017-04-26 20:01:02 +0300

    add lines in skils circle

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function () {
-  console.log("Дерьмо!");
+  
+  'use strict'
+
+  var d          = document,
+      skilsLines = d.getElementsByClassName('percent-lines'),
+
+      /* Methods */
+      addDiv =  function () { 
+        var div = d.createElement('div');
+        return  div;
+      };
+
+  for (var i = 0; i < skilsLines.length; i++ ) {
+
+    for (var j = 0; j < 24; j++ ) {
+      skilsLines[i].appendChild(addDiv());
+    }
+  }
+
 });
+
